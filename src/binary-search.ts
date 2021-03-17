1be60c83a6474fdaa2d804865f79369bca7bdb42
Lutogin <lutogin@gmail.com>
2021-03-18 01:00:13 +0200

    Add binary-search

---
+++ src/binary-search.ts
@@ -0,0 +1,23 @@
+const testArr: number[] = Array.from(Array(1024).keys());
+
+function binarySearch(arr: number[], search: number): number | null {
+  let indexMin = 0;
+  let indexMax = arr.length-1;
+
+  while (indexMin <= indexMax) {
+    const indexMid = Math.floor((indexMin + indexMax) / 2);
+    const currentValue = arr[indexMid];
+
+    if (currentValue === search) {
+      return indexMid;
+    } else if (currentValue > search) {
+      indexMax = indexMid - 1;
+    } else if (currentValue < search) {
+      indexMin = indexMid + 1;
+    }
+  }
+
+  return null;
+}
+
+console.log('binarySearch result---> ', binarySearch(testArr, 100));
