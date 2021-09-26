84ddc7eb42ae84f33c7ef666490acd42103e3cfb
Lutogin <lutogin@gmail.com>
2021-09-26 21:58:10 +0300

    Fix algorithm with create queue from graph

--- src/breadth-search.js
+++ src/breadth-search.js
@@ -1,56 +1,66 @@
-const graph = {
+const graph = [{
   name: 'you',
+  flagged: false,
   edge: [
     {
       name: 'alice',
+      flagged: false,
       edge: [
         {
           name: 'peggy',
+          flagged: false,
           edge: [],
         },
       ]
     },
     {
       name: 'bob',
+      flagged: false,
       edge: [
         {
           name: 'anuj',
+          flagged: false,
           edge: [],
         },
         {
-          name: 'anuj',
+          name: 'anu2',
+          flagged: true,
           edge: [],
         }
       ]
     },
     {
       name: 'claire',
+      flagged: false,
       edge: [
         {
           name: 'jonny',
+          flagged: false,
           edge: [
             {
               name: 'sally',
+              flagged: false,
               edge: [],
             },
           ],
         },
         {
           name: 'thom',
+          flagged: false,
           edge: [],
         }
       ]
     }
   ],
-};
+}];
 
 function makeQueue(data) {
-  if (!!data.edge.length) {
+  if (data.edge.length) {
     const result = [data.edge.map((el) => el.name)];
 
     const deepQueue = [];
     data.edge.forEach((el) => {
-      if (!!el.edge.length) {
+      if (el.edge.length) {
         deepQueue.push(...makeQueue(el));
       }
     });
@@ -63,6 +73,34 @@ function makeQueue(data) {
   }
 }
 
-const queue = makeQueue(graph);
+function makeQueue2(data, levelCounter = 0) {
+  const mainQueue = data.map((el) => ({
+    name: el.name,
+    flagged: el.flagged,
+    levelCounter,
+  }));
+
+  const dataToRecursionCall = [];
+  data.forEach((el) => {
+    if (el.edge.length) {
+      dataToRecursionCall.push(el.edge);
+    }
+  });
+
+  if (!dataToRecursionCall.length) {
+    return mainQueue;
+  }
+
+  return mainQueue.concat(makeQueue2(
+    dataToRecursionCall.flat(1),
+    levelCounter + 1,
+  ));
+}
+
+const queue = makeQueue2(graph);
 
 console.log('queue ---> ', queue);
+
+const foundFlaggedPerson = queue.find((el) => el.flagged);
+
+console.log('Flagged person on', foundFlaggedPerson.levelCounter, 'position');
--- src/quick-sort.js
+++ src/quick-sort.js
@@ -7,18 +7,18 @@ function quickSort(arr) {
 
   const pivot = arr[0]; // default use 1st el
   const lower = [];
-  const biger = [];
+  const bigger = [];
 
   for (let i = 1; i < arr.length; i++) {
     counter += 1;
     if (arr[i] < pivot) { // Because we are take 1st element as a pivot.
       lower.push(arr[i]);
     } else {
-      biger.push(arr[i]);
+      bigger.push(arr[i]);
     }
   }
 
-  return quickSort(lower).concat(pivot, quickSort(biger));
+  return quickSort(lower).concat(pivot, quickSort(bigger));
 }
 
 console.log(quickSort(([21, 333, 4, 76, 3, 12, 677, 2, 1])));
