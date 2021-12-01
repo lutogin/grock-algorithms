3c291b16e115248a8ae3533232b41eef9fd1741f
Lutogin <lutogin@gmail.com>
2021-12-02 00:01:51 +0200

    Added tree sum2

---
+++ src/tree-sum2.js
@@ -0,0 +1,38 @@
+const tree = {
+  valueNode: 3,
+  next: [{
+    valueNode: 1,
+    next: null
+  },
+  {
+    valueNode: 3,
+    next: null
+  },
+  {
+    valueNode: 2,
+    next: null
+  },
+  {
+    valueNode: 2,
+    next: [
+      {
+        valueNode: 1,
+        next: null
+      },
+      {
+        valueNode: 5,
+        next: null
+      }
+    ]
+  }],
+};
+
+function sumOfTree(t) {
+  return t.valueNode + t.next.reduce((sum, v) => {
+    if (v.next) {
+      return sum += sumOfTree(v);
+    } else {
+      return sum += v.valueNode;
+    }
+  }, 0);
+}
